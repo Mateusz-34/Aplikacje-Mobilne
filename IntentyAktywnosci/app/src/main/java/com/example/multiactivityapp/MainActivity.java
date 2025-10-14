4bb6a329d9017f61980ad105d950c3384c0ad11f
Mateusz-34 <mateusz.ostrozka34@gmail.com>
2025-10-14 18:31:01 +0200

    IntentyAktywnosci

---
+++ IntentyAktywnosci/app/src/main/java/com/example/multiactivityapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.intentyaktywnosci;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String IntentName = "name";
+    public static final String IntentSurname = "surname";
+    public static final String IntentEmail = "email";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textViewName = findViewById(R.id.textViewName);
+        TextView textViewSurname = findViewById(R.id.textViewSurname);
+        TextView textViewEmail = findViewById(R.id.textViewEmail);
+
+        Button buttonNavigate = findViewById(R.id.buttonNavigate);
+
+        buttonNavigate.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        });
+
+        String name = getIntent().getStringExtra(IntentName);
+        String surname = getIntent().getStringExtra(IntentSurname);
+        String email = getIntent().getStringExtra(IntentEmail);
+
+        if (name != null) {
+            textViewName.setText("Pole Name: " + name);
+        }
+        if (surname != null) {
+            textViewSurname.setText("Pole Surname: " + surname);
+        }
+        if (email != null) {
+            textViewEmail.setText("Pole Email: " + email);
+        }
+    }
+}
---
+++ IntentyAktywnosci/app/src/main/java/com/example/multiactivityapp/SecondActivity.java
@@ -0,0 +1,47 @@
+package com.example.intentyaktywnosci;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        EditText Name = findViewById(R.id.Name);
+        EditText Surname = findViewById(R.id.Surname);
+        EditText Email = findViewById(R.id.Email);
+
+        String messageName = getIntent().getStringExtra("messageName");
+        String messageSurname = getIntent().getStringExtra("messageSurname");
+        String messageEmail = getIntent().getStringExtra("messageEmail");
+
+        Name.setText(messageName);
+        Surname.setText(messageSurname);
+        Email.setText(messageEmail);
+
+        Button submit = findViewById(R.id.submit);
+        submit.setOnClickListener(v-> {
+            String messagename = Name.getText().toString();
+            String messagesurname = Surname.getText().toString();
+            String messageemail = Email.getText().toString();
+
+            Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+            intent.putExtra("name",messagename);
+            intent.putExtra("surname",messagesurname);
+            intent.putExtra("email",messageemail);
+
+            startActivity(intent);
+            finish();
+        });
+
+        Button buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(v -> finish());
+    }
+}
