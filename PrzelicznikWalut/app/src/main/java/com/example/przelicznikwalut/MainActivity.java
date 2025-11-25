e069454d523c8c2e91033f32c14c33fbf0eefd0d
Mateusz-34 <mateusz.ostrozka34@gmail.com>
2025-11-25 20:03:17 +0100

    PrzelicznikWalut

---
+++ PrzelicznikWalut/app/src/main/java/com/example/przelicznikwalut/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.przelicznikwalut;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner fromCurrencySpinner;
+    private Spinner toCurrencySpinner;
+    private Button convertButton;
+    private EditText amountInput;
+    private TextView resultText;
+
+    private final String[] currencies = {"PLN", "EUR", "USD", "GBP"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fromCurrencySpinner = findViewById(R.id.fromCurrencySpinner);
+        toCurrencySpinner = findViewById(R.id.toCurrencySpinner);
+        convertButton = findViewById(R.id.convertButton);
+        amountInput = findViewById(R.id.amountInput);
+        resultText = findViewById(R.id.resultText);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, currencies);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        fromCurrencySpinner.setAdapter(adapter);
+        toCurrencySpinner.setAdapter(adapter);
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                convertCurrency();
+            }
+        });
+    }
+    private void convertCurrency() {
+        String amountStr = amountInput.getText().toString();
+
+        if (amountStr.isEmpty()) {
+            Toast.makeText(this, "Wprowadź kwotę", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        double amount = Double.parseDouble(amountStr);
+
+        String fromCurrency = fromCurrencySpinner.getSelectedItem().toString();
+        String toCurrency = toCurrencySpinner.getSelectedItem().toString();
+
+        double EUR = 4.30;
+        double USD = 4.00;
+        double GBP = 5.00;
+
+        double inPln = 0;
+        double result = 0;
+
+        if (fromCurrency.equals("PLN")) inPln = amount;
+        else if (fromCurrency.equals("EUR")) inPln = amount * EUR;
+        else if (fromCurrency.equals("USD")) inPln = amount * USD;
+        else if (fromCurrency.equals("GBP")) inPln = amount * GBP;
+
+        if (toCurrency.equals("PLN")) result = inPln;
+        else if (toCurrency.equals("EUR")) result = inPln / EUR;
+        else if (toCurrency.equals("USD")) result = inPln / USD;
+        else if (toCurrency.equals("GBP")) result = inPln / GBP;
+
+        resultText.setText(String.format("%.2f %s", result, toCurrency));
+    }
+}
