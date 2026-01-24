f70fd55106d7a6e6e6f97b74ba768a16e544832e
Mateusz Ostrozka <mateusz.ostrozka34@gmail.com>
2026-01-24 16:26:31 +0100

    SkalowanieObrazka

---
+++ SkalowanieObrazka/app/src/main/java/com/example/skalowanieobrazka/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.skalowanieobrazka;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        SeekBar seekBar = findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                float scale = progress / 50.0f;
+                imageView.setScaleX(scale);
+                imageView.setScaleY(scale);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { 
+
+            }
+        });
+    }
+}
