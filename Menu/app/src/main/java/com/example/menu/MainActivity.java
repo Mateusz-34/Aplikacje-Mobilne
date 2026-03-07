9404d92d27262c7f4ee10900b83847eba341ea10
Mateusz Ostrozka <mateusz.ostrozka34@gmail.com>
2026-03-07 10:40:47 +0100

    Resetowanie licznika

--- Menu/app/src/main/java/com/example/menu/MainActivity.java
+++ Menu/app/src/main/java/com/example/menu/MainActivity.java
@@ -10,13 +10,16 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView infoText;
+    private TextView counterText;
     private View mainLayout;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +30,14 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         infoText = findViewById(R.id.infoText);
+        counterText = findViewById(R.id.counterText);
         mainLayout = findViewById(R.id.mainLayout);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            counter++;
+            counterText.setText(String.valueOf(counter));
+        });
     }
 
     @Override
@@ -46,11 +56,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         else if (id == R.id.action_info) {
-            if (infoText != null) {
-                infoText.setText("Aplikacja stworzona przez: Jana Kowalskiego");
-            } else {
-                Toast.makeText(this, "Aplikacja stworzona przez: Jana Kowalskiego", Toast.LENGTH_SHORT).show();
-            }
+            infoText.setText("Aplikacja stworzona przez: Jana Kowalskiego");
             return true;
         }
         else if (id == R.id.action_favorite) {
@@ -71,6 +77,11 @@ public class MainActivity extends AppCompatActivity {
         }
         else if (id == R.id.action_blue) {
             mainLayout.setBackgroundColor(Color.BLUE);
+            return true;
+        }
+        else if (id == R.id.action_reset) {
+            counter = 0;
+            counterText.setText("0");
             return true;
         }
 
