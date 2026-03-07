4da50f0eb073b2e27e6647f1c09ad7b6a7a484ae
Mateusz Ostrozka <mateusz.ostrozka34@gmail.com>
2026-03-07 10:30:11 +0100

    Ikona 'Ulubione' na pasku

---
+++ Menu/app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.menu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView infoText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        infoText = findViewById(R.id.infoText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "Wybrano Ustawienia", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if (id == R.id.action_info) {
+            if (infoText != null) {
+                infoText.setText("Aplikacja stworzona przez: Jana Kowalskiego");
+            } else {
+                Toast.makeText(this, "Aplikacja stworzona przez: Jana Kowalskiego", Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+        else if (id == R.id.action_favorite) {
+            Toast.makeText(this, "Dodano do ulubionych", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if (id == R.id.action_exit) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
