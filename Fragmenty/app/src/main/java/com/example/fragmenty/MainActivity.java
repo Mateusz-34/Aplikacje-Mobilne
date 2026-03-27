398b15c1192c5189b0f409a9fe02acf2fe8f33fd
Mateusz Ostrozka <mateusz.ostrozka34@gmail.com>
2026-03-27 16:38:25 +0100

    Fragmenty

---
+++ Fragmenty/app/src/main/java/com/example/fragmenty/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.fragmenty;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnFragment1, btnFragment2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFragment1 = findViewById(R.id.btnFragment1);
+        btnFragment2 = findViewById(R.id.btnFragment2);
+
+        loadFragment(new FirstFragment());
+
+        btnFragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FirstFragment());
+            }
+        });
+
+        btnFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SecondFragment());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragment_container, fragment);
+        ft.commit();
+    }
+}
