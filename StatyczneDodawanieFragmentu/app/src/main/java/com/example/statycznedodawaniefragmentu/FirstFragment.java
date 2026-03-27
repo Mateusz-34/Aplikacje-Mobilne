cdd7e77e7f2cc018398398d57ef3073e575654ab
Mateusz Ostrozka <mateusz.ostrozka34@gmail.com>
2026-03-27 17:30:28 +0100

    Statyczne dodawanie Fragmentu

---
+++ StatyczneDodawanieFragmentu/app/src/main/java/com/example/statycznedodawaniefragmentu/FirstFragment.java
@@ -0,0 +1,22 @@
+package com.example.statycznedodawaniefragmentu;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+
+    private TextView textView;
+
+    public FirstFragment() {
+        super(R.layout.fragment_first);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        textView = view.findViewById(R.id.textViewFragment);
+        textView.setText("To jest statyczny fragment");
+    }
+}
