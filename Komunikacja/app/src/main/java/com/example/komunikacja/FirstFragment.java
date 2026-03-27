d08cfc3f2fa967cc34d28e56a5536475c8c8084e
Mateusz Ostrozka <mateusz.ostrozka34@gmail.com>
2026-03-27 17:26:52 +0100

    Komunikacja Aktywność -> Fragment

--- Komunikacja/app/src/main/java/com/example/komunikacja/FirstFragment.java
+++ Komunikacja/app/src/main/java/com/example/komunikacja/FirstFragment.java
@@ -1,39 +1,25 @@
 package com.example.komunikacja;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class FirstFragment extends Fragment {
 
-    interface OnButtonClickedListener {
-        void onButtonClick();
-    }
-
-    OnButtonClickedListener listener;
+    private TextView textView;
 
     public FirstFragment() {
         super(R.layout.fragment_first);
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        listener = (OnButtonClickedListener) context;
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        textView = view.findViewById(R.id.textViewFragment);
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        Button btn = view.findViewById(R.id.btnInsideFragment);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                listener.onButtonClick();
-            }
-        });
+    public void updateText(String newText) {
+        textView.setText(newText);
     }
 }
--- Komunikacja/app/src/main/java/com/example/komunikacja/MainActivity.java
+++ Komunikacja/app/src/main/java/com/example/komunikacja/MainActivity.java
@@ -1,21 +1,40 @@
 package com.example.komunikacja;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity implements FirstFragment.OnButtonClickedListener {
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = findViewById(R.id.editTextMain);
+        button = findViewById(R.id.buttonSend);
+
         loadFragment(new FirstFragment());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+                if (fragment instanceof FirstFragment) {
+                    ((FirstFragment) fragment).updateText(text);
+                }
+            }
+        });
     }
 
     private void loadFragment(Fragment fragment) {
@@ -23,9 +42,5 @@ public class MainActivity extends AppCompatActivity implements FirstFragment.OnB
         FragmentTransaction ft = fm.beginTransaction();
         ft.replace(R.id.fragment_container, fragment);
         ft.commit();
-    }
-    @Override
-    public void onButtonClick() {
-        Toast.makeText(this, "Kliknięto przycisk we fragmencie!", Toast.LENGTH_SHORT).show();
     }
 }
