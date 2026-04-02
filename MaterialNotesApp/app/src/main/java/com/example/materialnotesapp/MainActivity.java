8cce705d47f7a3b6e20ae3141392ccf4ae8b0996
Mateusz Ostrozka <mateusz.ostrozka34@gmail.com>
2026-04-02 12:00:42 +0200

    Walidacja błędu z akcją cofania

--- MaterialNotesApp/app/src/main/java/com/example/materialnotesapp/MainActivity.java
+++ MaterialNotesApp/app/src/main/java/com/example/materialnotesapp/MainActivity.java
@@ -3,19 +3,17 @@ package com.example.materialnotesapp;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextInputEditText titleInput, contentInput;
+    private TextInputLayout titleLayout;
     private FloatingActionButton floatingActionButton;
 
     @Override
@@ -23,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        titleLayout = findViewById(R.id.titleLayout);
         titleInput = findViewById(R.id.titleInput);
         contentInput = findViewById(R.id.contentInput);
         floatingActionButton = findViewById(R.id.floatingActionButton);
@@ -30,14 +29,22 @@ public class MainActivity extends AppCompatActivity {
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String title = titleInput.getText().toString();
-                String content = contentInput.getText().toString();
+                String title = titleInput.getText().toString().trim();
+                String content = contentInput.getText().toString().trim();
+
+                if (title.isEmpty()) {
+                    titleLayout.setError("Tytuł nie może być pusty!");
+                    return;
+                } else {
+                    titleLayout.setError(null);
+                }
 
                 Snackbar.make(view, "Zapisano: " + title, Snackbar.LENGTH_LONG)
                         .setAction("COFNIJ", new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
                                 titleInput.setText("");
+                                contentInput.setText("");
                             }
                         })
                         .show();
