d256cc4e165a745ecfa4577e938351d6ace6e6f6
Mateusz Ostrozka <mateusz.ostrozka34@gmail.com>
2026-04-12 16:26:40 +0200

    MultiAutoCompleteTextView

--- WyborImienia/app/src/main/java/com/example/wyborimienia/MainActivity.java
+++ WyborImienia/app/src/main/java/com/example/wyborimienia/MainActivity.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AutoCompleteTextView autoCompleteCountry;
+    private MultiAutoCompleteTextView multiAutoComplete;
     private Button btnConfirm;
 
     @Override
@@ -18,20 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        autoCompleteCountry = findViewById(R.id.autoCompleteCountry);
+        multiAutoComplete = findViewById(R.id.multiAutoComplete);
         btnConfirm = findViewById(R.id.btnConfirm);
 
         String[] names = getResources().getStringArray(R.array.name_array);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, names);
-        autoCompleteCountry.setAdapter(adapter);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                names
+        );
+
+        multiAutoComplete.setAdapter(adapter);
+        multiAutoComplete.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
 
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String selectedCountry = autoCompleteCountry.getText().toString();
+                String selectedNames = multiAutoComplete.getText().toString();
                 Toast.makeText(MainActivity.this,
-                        "Wybrano: " + selectedCountry,
+                        "Wybrano: " + selectedNames,
                         Toast.LENGTH_SHORT).show();
             }
         });
